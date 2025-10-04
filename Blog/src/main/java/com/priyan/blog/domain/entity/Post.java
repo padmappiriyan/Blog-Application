e4156e54207d8ec8afcbeec047f3f340d5174d52
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-04 11:40:13 +0530

    Refactor Post entity to include category and tags relationships with JPA annotations

--- Blog/src/main/java/com/priyan/blog/domain/entity/Post.java
+++ Blog/src/main/java/com/priyan/blog/domain/entity/Post.java
@@ -5,7 +5,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -37,11 +39,24 @@ public class Post {
     @JoinColumn(name = "author_id" , nullable = false)
     private User author;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToMany
+    @JoinTable(
+            name = "post_tags",
+            joinColumns =  @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name= "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
+
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
 
     @Override
     public boolean equals(Object o) {
