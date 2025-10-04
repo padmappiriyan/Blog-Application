30658277a54a8f4bf9a0e5fcad4c49ba7578cf45
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-04 13:25:12 +0530

    Add CategoryService interface for category management

---
+++ Blog/src/main/java/com/priyan/blog/services/CategoryService.java
@@ -0,0 +1,9 @@
+package com.priyan.blog.services;
+
+import com.priyan.blog.domain.entity.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category>  listCategories();
+}
