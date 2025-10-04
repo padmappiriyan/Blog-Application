75c6820a7512abf6a73639f33399fd2ea176408f
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-04 13:25:40 +0530

    Add custom query to CategoryRepository for fetching categories with post count

--- Blog/src/main/java/com/priyan/blog/repositories/CategoryRepository.java
+++ Blog/src/main/java/com/priyan/blog/repositories/CategoryRepository.java
@@ -2,11 +2,15 @@ package com.priyan.blog.repositories;
 
 import com.priyan.blog.domain.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    @Query("SELECT c FROM Category c LEFT JOIN FETCH c.posts")
+    List<Category> findAllPostCount();
 
 }
