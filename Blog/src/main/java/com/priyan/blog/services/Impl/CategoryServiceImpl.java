9499cc4a5d1df23a50034de14957e5b2c2d03a9e
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-04 13:25:21 +0530

    Implement CategoryService interface with CategoryServiceImpl for category management

---
+++ Blog/src/main/java/com/priyan/blog/services/Impl/CategoryServiceImpl.java
@@ -0,0 +1,19 @@
+package com.priyan.blog.services.Impl;
+
+import com.priyan.blog.domain.entity.Category;
+import com.priyan.blog.repositories.CategoryRepository;
+import com.priyan.blog.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+   private final CategoryRepository categoryRepository;
+    @Override
+    public List<Category> listCategories() {
+        return categoryRepository.findAllPostCount();
+    }
+}
