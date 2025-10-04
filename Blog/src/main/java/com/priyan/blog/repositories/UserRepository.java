e25d0e0cd51d89c12f617e1a37b28feceaddfe7d
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-04 11:54:17 +0530

    Add UserRepository interface for User entity management

---
+++ Blog/src/main/java/com/priyan/blog/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.priyan.blog.repositories;
+
+import com.priyan.blog.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, UUID> {
+
+}
