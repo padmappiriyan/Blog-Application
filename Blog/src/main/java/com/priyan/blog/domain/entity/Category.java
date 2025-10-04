1ecad8d0243f99d372a23a5bc3f5306e88552932
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-04 11:40:01 +0530

    Add posts relationship to Category entity with JPA annotations

--- Blog/src/main/java/com/priyan/blog/domain/entity/Category.java
+++ Blog/src/main/java/com/priyan/blog/domain/entity/Category.java
@@ -3,6 +3,8 @@ package com.priyan.blog.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -20,6 +22,9 @@ public class Category {
 
     @Column(nullable=false, unique=true)
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Post> posts = new ArrayList<>();
 
     @Override
     public boolean equals(Object o) {
