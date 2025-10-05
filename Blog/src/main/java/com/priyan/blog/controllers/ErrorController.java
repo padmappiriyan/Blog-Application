3ea0ae00ef04708d0782d8a8a254430238b91a50
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-05 22:43:25 +0530

    Add ErrorController to handle exceptions and return API error responses

---
+++ Blog/src/main/java/com/priyan/blog/controllers/ErrorController.java
@@ -0,0 +1,36 @@
+package com.priyan.blog.controllers;
+
+import com.priyan.blog.domain.dtos.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception ex) {
+         log.error("Caught an exception", ex);
+         ApiErrorResponse error =ApiErrorResponse.builder()
+                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                 .message("An Un expected error occured")
+                 .build();
+         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+
+
+
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+                ApiErrorResponse error= ApiErrorResponse.builder()
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .message(ex.getMessage())
+                        .build();
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
