3e60b305e0e14a8c469dd6bd9588dbe4fbfef9b1
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-05 16:25:25 +0530

    Refactor CategoryController to improve imports and add createCategory method for category creation

--- Blog/src/main/java/com/priyan/blog/controllers/CategoryController.java
+++ Blog/src/main/java/com/priyan/blog/controllers/CategoryController.java
@@ -2,14 +2,15 @@ package com.priyan.blog.controllers;
 
 
 import com.priyan.blog.domain.dtos.CategoryDto;
+import com.priyan.blog.domain.dtos.CreateCategoryRequest;
 import com.priyan.blog.domain.entity.Category;
 import com.priyan.blog.mappers.CategoryMapper;
 import com.priyan.blog.services.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +27,14 @@ public class CategoryController {
                 .stream().map(categoryMapper::toDto)
                 .toList();
         return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CreateCategoryRequest createCategoryRequest) {
+            Category categoryToCreate=categoryMapper.toEntity(createCategoryRequest);
+            Category savedCategory=categoryService.createCategory(categoryToCreate);
+            CategoryDto categoryDto = categoryMapper.toDto(savedCategory);
+            return ResponseEntity.status(HttpStatus.CREATED).body(categoryDto);
+
     }
 }
