dbd7a631cffbdb3f4545e8f559f2773a39830314
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-07 00:01:55 +0530

    Add AuthenticationService

---
+++ Blog/src/main/java/com/priyan/blog/services/AuthenticationService.java
@@ -0,0 +1,11 @@
+package com.priyan.blog.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AuthenticationService {
+    UserDetails authenticate(String email, String password);
+    String generateToken(UserDetails userDetails);
+
+}
