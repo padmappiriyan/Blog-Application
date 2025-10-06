c67867e4ffdd7a2af8178b0b90e7b406e7a5957c
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-06 12:06:06 +0530

    Add deleteCategory method to CategoryService

--- Blog/src/main/java/com/priyan/blog/services/CategoryService.java
+++ Blog/src/main/java/com/priyan/blog/services/CategoryService.java
@@ -3,8 +3,11 @@ package com.priyan.blog.services;
 import com.priyan.blog.domain.entity.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
     List<Category>  listCategories();
     Category createCategory(Category category);
+    void deleteCategory(UUID id);
+
 }
