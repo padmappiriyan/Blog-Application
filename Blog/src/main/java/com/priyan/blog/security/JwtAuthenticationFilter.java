eae1e10474e12cdf14787e44da6eaa76267b2c3b
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-08 21:00:57 +0530

    Refactor JwtAuthenticationFilter to use AuthenticationService for token validation and improve error handling

--- Blog/src/main/java/com/priyan/blog/security/JwtAuthenticationFilter.java
+++ Blog/src/main/java/com/priyan/blog/security/JwtAuthenticationFilter.java
@@ -1,25 +1,48 @@
 package com.priyan.blog.security;
 
+import com.priyan.blog.services.AuthenticationService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final UserDetailsService userDetailsService;
+    private final AuthenticationService authenticationService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-
-        String token=extractToken(request);
+try {
+    String token = extractToken(request);
+    if (token != null) {
+        UserDetails userDetails = authenticationService.validateToken(token);
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (userDetails instanceof BlogUserDetails) {
+            request.setAttribute("userId", ((BlogUserDetails) userDetails).getId());
+        }
+    }
+}
+catch(Exception ex) {
+    //Do not throw exception, just don't authenticate the user
+    log.warn("Received invalid auth token");
+}
+    filterChain.doFilter(request, response);
 
     }
 
