75302c84d1428972d22fbcf2564b5402f3766a6c
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-09 19:13:55 +0530

    create router and routes for url

--- frontend_user/src/App.jsx
+++ frontend_user/src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
-
+import LoginPage from './pages/LoginPage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className=''>
-     <Navbar/>
-    </div>
+    
+    
+
+      <Router>
+        <div className="h-dvh flex flex-col">
+        <Navbar />
+      <main className='flex-grow'>
+        <Routes >
+       
+        <Route path="/login" element={<LoginPage />} />
+     
+        </Routes>
+      </main>
+      {/* <Footer /> */}
+      <Footer />
+      </div>
+    </Router>
+
+   
+     
+    
   )
 }
 
