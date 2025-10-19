8e226a0697231a08d3e87305cb1decf2b45e1471
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-10-19 20:50:41 +0530

    Update AuthController

--- Blog/src/main/java/com/priyan/blog/controllers/AuthController.java
+++ Blog/src/main/java/com/priyan/blog/controllers/AuthController.java
@@ -6,13 +6,10 @@ import com.priyan.blog.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api/v1/auth")
+@RequestMapping(path = "/api/v1/auth/login")
 @RequiredArgsConstructor
 public class AuthController {
         private final AuthenticationService authenticationService;
